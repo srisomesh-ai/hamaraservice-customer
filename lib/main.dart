37260bd53e148d30fe05813d70a4888572d9347f
srisomesh-ai <somesh9346220090@gmail.com>
2026-06-12 12:26:47 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'firebase_options.dart';
 import 'utils/theme.dart';
 import 'screens/splash_screen.dart';
-import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true, badge: true, sound: true, criticalAlert: true,
     );
-  } catch (e) {
-    debugPrint('Firebase init error: $e');
-  }
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true,
+    );
+
+    // Save FCM token for customer
+    FirebaseAuth.instance.authStateChanges().listen((user) async {
+      if (user != null) {
+        try {
+          final token = await FirebaseMessaging.instance.getToken();
+          if (token != null) {
+            await FirebaseDatabase.instance.ref('customers/${user.uid}/fcmToken').set(token);
+          }
+        } catch (e) {}
+      }
+    });
+
+    // Handle notification tap
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      // Navigate to bookings when notification tapped
+    });
+
+  } catch (e) {}
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const HamaraServiceApp());
+  runApp(const CustomerApp());
 }
 
-class HamaraServiceApp extends StatelessWidget {
-  const HamaraServiceApp({super.key});
-
+class CustomerApp extends StatelessWidget {
+  const CustomerApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

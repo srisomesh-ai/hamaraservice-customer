27119fa85c65ea989792aef922de400bbdb8be9a
srisomesh-ai <somesh9346220090@gmail.com>
2026-06-14 08:53:24 +0200

    feat: add vibration on provider accepted + haptic on all actions

--- lib/screens/dashboard/my_bookings_screen.dart
+++ lib/screens/dashboard/my_bookings_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:vibration/vibration.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:url_launcher/url_launcher.dart';

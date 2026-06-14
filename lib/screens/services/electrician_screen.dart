42093cfcc003a556bc36813c279bff89175909ea
srisomesh-ai <somesh9346220090@gmail.com>
2026-06-14 17:38:46 +0200

    feat: SVC020 dynamic prices from Firebase

--- lib/screens/services/electrician_screen.dart
+++ lib/screens/services/electrician_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../utils/theme.dart';
 import 'service_widgets.dart';
+import '../../services/service_price_service.dart';
 
 class ElectricianScreen extends StatefulWidget {
   const ElectricianScreen({super.key});
@@ -9,10 +10,11 @@ class ElectricianScreen extends StatefulWidget {
 class _ElectricianState extends State<ElectricianScreen> {
   final Set<String> _issues = {};
   final _px = {'switch':399,'fan':399,'wiring':599,'mcb':499,'socket':399,'short':699,'light':299,'exhaust':349};
-  int get _total {
-    if (_issues.isEmpty) return 399;
-    return _issues.fold(0,(s,k)=>s+_px[k]!);
+  int _getTotal() {
+    if (_issues.isEmpty) return _p('switch');
+    return _issues.fold(0,(s,k)=>s+_p(k));
   }
+  int get _total => _getTotal();
   bool get _any => _issues.isNotEmpty;
   List<String> get _summary => [
     if (_issues.isEmpty) 'Electrician > General Visit',
